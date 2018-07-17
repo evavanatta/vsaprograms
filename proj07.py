f8bbbbea92f9b282354ce0a1daafbf33ea66e11f
evavanatta <40904308+evavanatta@users.noreply.github.com>
2018-07-17 09:56:00 -0500

    Add files via upload

---
+++ proj07.py
@@ -0,0 +1,280 @@
+# Name:
+# Date:
+
+# proj07: Word Game
+
+import random
+import string
+
+VOWELS = 'aeiou'
+CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+HAND_SIZE = 7
+score = 0
+n = 7
+
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k':
+        5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u':
+        1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+
+WORDLIST_FILENAME = "words.txt"
+
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    # wordlist: list of strings
+    wordlist = []
+    for line in inFile:
+        wordlist.append(line.strip().lower())
+    print "  ", len(wordlist), "words loaded."
+    return wordlist
+
+def get_frequency_dict(sequence):
+    """
+    Returns a dictionary where the keys are elements of the sequence
+    and the values are integer counts, for the number of times that
+    an element is repeated in the sequence.
+
+    sequence: string or list
+    return: dictionary
+    """
+    # freqs: dictionary (element_type -> int)
+    freq = {}
+    for x in sequence:
+        freq[x] = freq.get(x,0) + 1
+    return freq
+	
+
+# (end of helper code)
+# -----------------------------------
+
+#
+# Problem #1: Scoring a word
+#
+def get_word_score(word, n):
+    score = 0
+    letters = 0
+    nn = n
+    word.lower()
+    for counter in word:
+        score = score + SCRABBLE_LETTER_VALUES.get(counter, 0)
+        letters = letters + 1
+        n = n - 1
+    score = score * letters
+    if letters == nn:
+        score = score + 50
+    return score
+
+# TO DO...
+    
+#
+# Make sure you understand how this function works and what it does!
+#
+def display_hand(hand):
+    """
+    Displays the letters currently in the hand.
+
+    For example:
+       display_hand({'a':1, 'x':2, 'l':3, 'e':1})
+    Should print out something like:
+       a x x l l l e
+    The order of the letters is unimportant.
+
+    hand: dictionary (string -> int)
+    """
+    for letter in hand.keys():
+        for j in range(hand[letter]):
+             print letter,              # print all on the same line
+    print                               # print an empty line
+
+#
+# Make sure you understand how this function works and what it does!
+#
+def deal_hand(n):
+    """
+    Returns a random hand containing n lowercase letters.
+    At least n/3 the letters in the hand should be VOWELS.
+
+    Hands are represented as dictionaries. The keys are
+    letters and the values are the number of times the
+    particular letter is repeated in that hand.
+
+    n: int >= 0
+    returns: dictionary (string -> int)
+    """
+    hand={}
+    num_vowels = n / 3
+    
+    for i in range(num_vowels):
+        x = VOWELS[random.randrange(0,len(VOWELS))]
+        hand[x] = hand.get(x, 0) + 1
+        
+    for i in range(num_vowels, n):    
+        x = CONSONANTS[random.randrange(0,len(CONSONANTS))]
+        hand[x] = hand.get(x, 0) + 1
+        
+    return hand
+
+#
+# Problem #2: Update a hand by removing letters
+#
+def update_hand(hand, word):
+    hand2 = hand.copy()
+    for let in word:
+        if let in hand2:
+            hand2[let] = hand2[let] - 1
+    return hand2
+
+
+
+
+
+    # TO DO ...
+
+#
+# Problem #3: Test word validity
+#
+def is_valid_word(word, hand, word_list):
+    hand2 = hand
+    if word in word_list:
+        for l in word:
+            if l in hand2 and hand2[l] != 0:
+                hand2[l] = hand2[l] - 1
+            else:
+                return False
+        return True
+    else:
+        return False
+
+
+
+
+
+    """
+    Returns True if word is in the word_list and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+    Does not mutate hand or word_list.
+    
+    word: string
+    hand: dictionary (string -> int)
+    word_list: list of lowercase strings
+    """
+    # TO DO...
+
+def calculate_handlen(hand):
+    handlen = 0
+    for v in hand.values():
+        handlen += v
+    return handlen
+
+#
+# Problem #4: Playing a hand
+#
+def play_hand(hand, word_list):
+    tscore = 0
+    while True:
+        print "Current Hand:",
+        display_hand(hand)
+        word = raw_input("Enter a word, or a '.' to indicate that you are finished: ")
+        if word == ".":
+            print
+            print "Total Score: " + str(tscore) + " points."
+            return 1
+        else:
+            if is_valid_word(word, hand, word_list) == True:
+                tscore = get_word_score(word, n) + tscore
+                update_hand(hand, word)
+                print str(word), "earned", str(get_word_score(word, n)), "points! Total", str(tscore) + "."
+                print
+            else:
+                print "Invalid Word"
+                print
+
+
+
+
+    """
+    Allows the user to play the given hand, as follows:
+
+    * The hand is displayed.
+    
+    * The user may input a word.
+
+    * An invalid word is rejected, and a message is displayed asking
+      the user to choose another word.
+
+    * When a valid word is entered, it uses up letters from the hand.
+
+    * After every valid word: the score for that word is displayed,
+      the remaining letters in the hand are displayed, and the user
+      is asked to input another word.
+
+    * The sum of the word scores is displayed when the hand finishes.
+
+    * The hand finishes when there are no more unused letters.
+      The user can also finish playing the hand by inputing a single
+      period (the string '.') instead of a word.
+
+      hand: dictionary (string -> int)
+      word_list: list of lowercase strings
+      
+    """
+    # TO DO ...
+
+#
+# Problem #5: Playing a game
+# Make sure you understand how this code works!
+# 
+def play_game(word_list):
+
+    while True:
+        q = raw_input("New, Retry, or Exit? (n, r, e) ")
+        if q == "n":
+            hand = deal_hand(HAND_SIZE)
+            phand = hand.copy()
+            play_hand(phand, word_list)
+        elif q == "r":
+            phand = hand.copy()
+            play_hand(phand, word_list)
+        elif q == "e":
+            break
+        else:
+            print "Please input a valid command."
+            print
+
+
+    """
+    Allow the user to play an arbitrary number of hands.
+
+    * Asks the user to input 'n' or 'r' or 'e'.
+
+    * If the user inputs 'n', let the user play a new (random) hand.
+      When done playing the hand, ask the 'n' or 'e' question again.
+
+    * If the user inputs 'r', let the user play the last hand again.
+
+    * If the user inputs 'e', exit the game.
+
+    * If the user inputs anything else, ask them again.
+    """
+    # TO DO...
+
+#
+# Build data structures used for entire session and play game
+#
+if __name__ == '__main__':
+    word_list = load_words()
+    play_game(word_list)
+
---
+++ proj07_extension.py
@@ -0,0 +1,212 @@
+# Name:
+# Date:
+
+# proj07 Extension
+
+from proj07 import *
+import time
+from perm import *
+word_list = load_words()
+
+#
+#
+# Problem #6A: Computer chooses a word
+#
+#
+
+
+
+def comp_choose_word(hand, word_list, difficulty):
+    counter = 0
+    cword = ""
+    get_perms(hand, n)
+    for word in word_list:
+        counter = 0
+        hand2 = hand.copy()
+        for l in word:
+            if difficulty == 1:
+                if len(word) >= 5:
+                    break
+            elif difficulty == 2:
+                if len(word) >= 6:
+                    break
+            if l in hand2 and hand2[l] != 0:
+                hand2[l] = hand2[l] - 1
+                counter = counter + 1
+            else:
+                break
+        if counter == len(word):
+            if len(word) > len(cword):
+                cword = word
+    if cword != "":
+        return cword
+    else:
+        return "."
+#
+# Problem #6B: Computer plays a hand
+#
+def comp_play_hand(hand, word_list, difficulty):
+
+    tcscore = 0
+    while True:
+        print "Current Hand:",
+        display_hand(hand)
+        comword = comp_choose_word(hand, word_list, difficulty)
+        print comword
+        if comword == ".":
+            print
+            print "Total Score: " + str(tcscore) + " points."
+            print
+            return tcscore
+        else:
+            if is_valid_word(comword, hand, word_list) == True:
+                tcscore = get_word_score(comword, n) + tcscore
+                update_hand(hand, comword)
+                print str(comword), "earned", str(get_word_score(comword, n)), "points! Total", str(tcscore) + "."
+                print
+            else:
+                print "Invalid Word"
+                print
+
+    """
+     Allows the computer to play the given hand, as follows:
+
+     * The hand is displayed.
+
+     * The computer chooses a word using comp_choose_words(hand, word_dict).
+
+     * After every valid word: the score for that word is displayed, 
+       the remaining letters in the hand are displayed, and the computer 
+       chooses another word.
+
+     * The sum of the word scores is displayed when the hand finishes.
+
+     * The hand finishes when the computer has exhausted its possible choices 
+        (i.e. comp_play_hand returns None).
+
+     hand: dictionary (string -> int)
+     word_list: list (string)
+    """
+    # TO DO ...    
+    
+#
+# Problem #6C: Playing a game
+#
+#
+def play_game(word_list):
+    phand = ""
+    while True:
+        y = 0
+        q = raw_input("New, Retry, or Exit? (n, r, e) ")
+        if q == "n":
+            hand = deal_hand(HAND_SIZE)
+            phand = hand.copy()
+            qu = raw_input("Computer or You? (c, u) ")
+            if qu == "u":
+                play_hand(phand, word_list)
+            if qu == "c":
+               print '''
+                                             _________
+                                           /         /.
+                    .-------------.       /_________/ |
+                   /             / |      |         | |
+                  /+============+\ |      | |====|  | |
+                  ||C:\>        || |      |         | |
+                  ||            || |      | |====|  | |
+                  ||            || |      |   ___   | |
+    --------------||            || |------|  |696|  | |--------
+                  ||            ||/@@@    |   ---   | |
+                  \+============+/    @   |_________|./.
+                                     @          ..  ....'
+                  ..................@     __.'.'  ''
+                 /oooooooooooooooo//     ///
+                /................//     /_/
+                ------------------
+                '''
+               print
+               difficulty = int(raw_input("Enter the difficulty you want(1=easy, 2=medium, 3=hard): "))
+               print
+               print
+               comp_play_hand(phand, word_list, difficulty)
+            else:
+                print "Please input a valid command."
+                print
+        elif q == "r" and phand != "":
+            phand = hand.copy()
+            qu = raw_input("Computer or You? (c , u)")
+            if qu == "u":
+                y = play_hand(phand, word_list)
+            if qu == "c":
+                print '''
+                                                                             _________
+                                                                           /         /.
+                                                    .-------------.       /_________/ |
+                                                   /             / |      |         | |
+                                                  /+============+\ |      | |====|  | |
+                                                  ||C:\>        || |      |         | |
+                                                  ||            || |      | |====|  | |
+                                                  ||            || |      |   ___   | |
+                                    --------------||            || |------|  |420|  | |-------
+                                                  ||            ||/@@@    |   ---   | |
+                                                  \+============+/    @   |_________|./.
+                                                                     @          ..  ....'
+                                                  ..................@     __.'.'  ''
+                                                 /oooooooooooooooo//     ///
+                                                /................//     /_/
+                                                ------------------
+                                                '''
+                print
+                difficulty = int(raw_input("Enter the difficulty you want(1=easy, 2=medium, 3=hard): "))
+                comp_play_hand(phand, word_list, difficulty)
+            else:
+                print "Please input a valid command."
+                print
+        elif q == "e":
+            print ">:C"
+            break
+        elif q != r or q != n or q != e:
+            print "Please input a valid command."
+            print
+
+
+
+    """Allow the user to play an arbitrary number of hands.
+
+    1) Asks the user to input 'n' or 'r' or 'e'.
+    * If the user inputs 'n', play a new (random) hand.
+    * If the user inputs 'r', play the last hand again.
+    * If the user inputs 'e', exit the game.
+    * If the user inputs anything else, ask them again.
+
+    2) Ask the user to input a 'u' or a 'c'.
+    * If the user inputs 'u', let the user play the game as before using play_hand.
+    * If the user inputs 'c', let the computer play the game using comp_play_hand 
+        (created above).
+    * If the user inputs anything else, ask them again.
+
+    3) After the computer or user has played the hand, repeat from step 1
+
+    word_list: list (string)
+    """
+    # TO DO...
+        
+#
+# Build data structures used for entire session and play game
+# #
+# if __name__ == '__main__':
+#     word_list = load_words()
+#     play_game(word_list)
+
+    
+print '''
+                               888     888     888         
+                               888     888     888         
+                               888     888     888         
+.d8888b  .d8888b888d888 8888b. 88888b. 88888b. 888 .d88b.  
+88K     d88P"   888P"      "88b888 "88b888 "88b888d8P  Y8b 
+"Y8888b.888     888    .d888888888  888888  88888888888888 
+     X88Y88b.   888    888  888888 d88P888 d88P888Y8b.     
+ 88888P' "Y8888P888    "Y88888888888P" 88888P" 888 "Y8888 
+ '''
+
+play_game(word_list)
